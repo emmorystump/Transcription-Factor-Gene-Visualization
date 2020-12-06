977d66fa6229cdbd457ff34e580cafc874921032
cmatkhan <chase.mateusiak@gmail.com>
2020-12-06 09:20:55 -0600

    fixing error handling chart -- this is fixing the update inputs issue

--- js/GoManhattenPlot.js
+++ js/GoManhattenPlot.js
@@ -1,10 +1,4 @@
 
-
-// ("<p>Description: " + i.description + "<br>"+
-//        "GO_term: " + i.native + "<br>"+
-//        "<p> p-value: " + i.p_value + "<\p>"+
-//        '<a class="nav-link" href="'+self.geneDetail.goUrlMaker(i.parents, sessionStorage.getItem("go_chart_url_prefix")) + '" target="_blank">GO term chart<\a>')
-
 /**
  * Constructor
  */
@@ -20,11 +14,11 @@ function GoManhattenPlot(colorScheme, geneDetail, goHeatmap, functional_categori
 
 /**
  * Initializes the svg elements required for this chart
- * TODO: put tooltip infrastructure somewhere w/common access -- less redundancy
  */
 GoManhattenPlot.prototype.init = function(){
     var self = this;
     self.margin = {top: 100, right: 100, bottom: 0, left: 50};
+    var go_categories = ["GO:BP", "GO:CC","GO:MF","KEGG"];
 
     //Gets access to the div element created for this chart from HTML
     self.divGoManhattenPlot = d3.select("#go-manhatten-plot").classed("content", true);
@@ -39,15 +33,20 @@ GoManhattenPlot.prototype.init = function(){
         .append("g")
         .attr("transform", "translate(" + self.margin.left + "," + self.margin.top + ")");
 
-    var go_categories = ["GO:BP", "GO:CC","GO:MF","KEGG"];
-    // Build X scales and axis:
+    // build x scales and axis, but do not attach to svg -- this is done in visualize()
     self.x = d3.scaleBand()
       .domain(go_categories)
-      .range([0, self.svgWidth]);
+      .range([0, self.svgWidth])
 
-    // Build X scales and axis:
+    // Build X scales and axis, but do not attach to svg -- this is done in visualize()
     self.y = d3.scaleLog()
-      .range([ 0, self.svgHeight-self.margin.top-self.margin.bottom ]);
+      .range([ 0, self.svgHeight-self.margin.top-self.margin.bottom ])
+      .clamp(true);
+
+    // size scale for the points in the plot -- domain set in visualize()
+    self.pointScale = d3.scaleLog()
+                        .range([8, 4])
+                        .clamp(true);
 
 }; // end init()
 
@@ -102,55 +101,56 @@ GoManhattenPlot.prototype.distributeGOdata = function(go_data_object){
 
 /**
  * visualize the GO data
- * @params the 'results' object of the returned data from the gProfilerGO
+ * @params go_object: the 'results' object of the returned data from the gProfilerGO
  */
 GoManhattenPlot.prototype.visualize = function(go_object){
-  // need to fix margin around svg, axis, etc
   var self = this;
+  // instantiate variables used in the loop to find the bounds of the pvalues
   var pval = [];
-
   // these are turned negative after finding min/max -- see next block
   // actually neg log10 -- the names of these variables got screwed up as i debugged. If this message isn't here, read VERY carefully and ignore the variable names
-  var min_Log10_pval = [1*10**6];
-  var min_index = [0];
-  var max_Log10_pval = [-1];
-  var max_index = [0];
+  var min_negLog10_pval = [1*10**6];
+  var min_pval_index = [0];
+  var max_negLog10_pval = [-1];
+  var max_pval_index = [0];
 
+  // if the go_object is empty, throw error which is printed to screen (this is handled at the VERY bottom of this function)
   try{
     if(go_object.length < 1){
       throw 'No significant GO terms. Try a less stringent threshold on the edge weights to increase the number of target genes.';
     } else{
+      // if there is no error, remove the notice to the user (the error above) in preparation of appending the graph
       $("#go-network-error").remove()
+      // figure out what the min/max pvalues are
       go_object.forEach((item, i) => {
         pval[0] = -1*Math.log(item.p_value);
-        if (pval[0] < min_Log10_pval[0]){
-          min_Log10_pval[0] = pval[0];
-          max_index[0] = i;
+        if (pval[0] < min_negLog10_pval[0]){
+          min_negLog10_pval[0] = pval[0];
+          max_pval_index[0] = i;
         }
-        if(pval[0] > max_Log10_pval[0]){
-          max_Log10_pval[0] = pval[0];
-          min_index[0] = i;
+        if(pval[0] > max_negLog10_pval[0]){
+          max_negLog10_pval[0] = pval[0];
+          min_pval_index[0] = i;
         }
-      });
+      }); // end p value min/max loop
+
+      // if the max_negLog10_pval hasn't been updated, throw and error (this not a public notice error like above)
       try{
-        if(max_Log10_pval == -1) throw "Error: no max pvalue found in GoManhattenPlot.visualize"
+        if(max_negLog10_pval == -1) throw "Error: no max pvalue found in GoManhattenPlot.visualize"
       } catch(err){
         console.error(err)
-      }
-
-        self.y.domain([go_object[min_index[0]].p_value, go_object[max_index[0]].p_value])
-              .clamp(true);
-
-        var pointScale = d3.scaleLog()
-                           .domain([go_object[min_index[0]].p_value, go_object[max_index[0]].p_value])
-                           .range([8, 4])
-                           .clamp(true)
+      } // end pvalue error check
 
       // remove all circles, if they exist, to clear graph for new data
-      $(".manhatten-circles").remove()
+      $(".manhatten-plot-instance").remove()
+      // set y scale domain
+      self.y.domain([go_object[min_pval_index[0]].p_value, go_object[max_pval_index[0]].p_value])
+      // set point size domain
+      self.pointScale.domain([go_object[min_pval_index[0]].p_value, go_object[max_pval_index[0]].p_value])
 
-      // append axis to svg object
+      // append x axis to svg object
       self.svg.append("g")
+        .classed("manhatten-plot-instance", true)
         .attr("transform", "translate(0," + (self.svgHeight-self.margin.top-self.margin.bottom) + ")")
         .attr("class", "x-axis")
         .call(d3.axisBottom(self.x))
@@ -173,19 +173,23 @@ GoManhattenPlot.prototype.visualize = function(go_object){
           } // end if
         }); // end onclick
 
-      self.svg.append("g")
-        .call(d3.axisLeft(self.y));
+      // append y axis to dom
+      self.svg
+          .classed("manhatten-plot-instance", true)
+          .append("g")
+          .call(d3.axisLeft(self.y));
 
+      // add circles to manhatten plot
       self.svg.selectAll("circle")
         .data(go_object)
         .enter()
         .append("circle")
         .attr("cx", function(d,i) { return self.x(d.source) + self.svgWidth/8 }) // TODO: THIS NEEDS TO BE SOMEHOW ADJUSTED BASED ON SCREEN SIZE? SOMETHING OTHER THAN HARD CODING
         .attr("cy", function(d,i) { return self.y(d.p_value) })
-        .attr("r", function(d,i) {return pointScale(d.p_value)} )
+        .attr("r", function(d,i) {return self.pointScale(d.p_value)} )
         .style("opacity", .4)
         .attr("fill", function(d,i) {return self.colorScheme(d.source)})
-        .attr("class", "manhatten-circles")
+        .attr("class", "manhatten-plot-instance")
         .attr("class", function(d,i) {return d.source})
         .on("click", function(node_info, data){
           self.geneDetail.updateGoDetail(data)
@@ -196,11 +200,15 @@ GoManhattenPlot.prototype.visualize = function(go_object){
         })
         .on("mouseleave", function(node_info, data){
           // return color to prev
-        });
-    }
+        }); // end circle append
+    } // end the else clause of the if statement inside the try clause at the top of the function
+  // catch the error re: go_object empty and print to screen as notice to user (not really an "error" -- nothing is broken)
   } catch(err){
-    console.error("here: " + go_object.length)
+    // clear graph and old notices
+    $(".manhatten-plot-instance").remove()
+    $("#go-network-error").remove()
+    // print the notice
     $("#go-manhatten-plot").append('<p id="go-network-error">'+err+'</p>');
-  } // end try .. catch
+  } // end try .. catch wrapping whole function
 
 }; // end visualize()
